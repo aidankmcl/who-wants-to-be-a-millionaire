3eebc73eb84d00381c092d51d4aaeaec08d56cfa
Aidan <aidankmcl@gmail.com>
2019-11-22 09:21:22 +0100

    State approach using questions as example

---
+++ src/state/index.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import {
+  IQuestionsState,
+  QuestionsAction,
+  initialQuestionsState,
+  questionsReducer,
+  questionsActions
+} from './questions';
+
+interface IGlobalState {
+  questions: IQuestionsState;
+}
+
+const initialState: IGlobalState = {
+  questions: initialQuestionsState,
+}
+
+type GlobalAction = QuestionsAction;
+
+const GlobalContext = React.createContext<[IGlobalState, React.Dispatch<GlobalAction>]>([
+  initialState,
+  () => null
+]);
+
+
+const globalReducer = (state: IGlobalState, action: GlobalAction): IGlobalState => {
+  const { questions } = state;
+
+  return {
+    questions: questionsReducer(questions, action)
+  }
+}
+
+const PreventRerender = React.memo(({ children }) => (<>{children}</>), () => true)
+
+export const Provider: React.FC = ({ children }) => {
+  const state = React.useReducer(globalReducer, initialState);
+
+  return (
+    <GlobalContext.Provider value={state}>
+      <PreventRerender>
+        {children}
+      </PreventRerender>
+    </GlobalContext.Provider>
+  )
+}
---
+++ src/state/questions.ts
@@ -0,0 +1,70 @@
+
+import { createAction, createAsyncAction, createReducer, ActionType } from 'typesafe-actions';
+
+import { shuffle } from '../utils/array';
+
+type Question = {
+  id: string;
+  text: string; // To have a question with no text, must use empty string
+  image?: string; // Link to image
+  answers: string[];
+  correct: number;
+}
+
+export interface IQuestionsState {
+  questions: Question[];
+  current: Question | null;
+  loading: boolean;
+}
+
+export const initialQuestionsState = {
+  questions: [],
+  current: null,
+  loading: false,
+}
+
+/*
+ * I'll pretend that I'm requesting from a service and use
+ * actions for each step to show how I'd handle the logic
+ * if I were.
+ */
+const requestQuestions = createAsyncAction(
+  'questions/RETRIEVE_REQUEST',
+  'questions/RETRIEVE_SUCCESS',
+  'questions/RETRIEVE_FAILURE',
+)<void, Question[], void>();
+
+const nextQuestion = createAction('questions/NEXT')();
+
+export const questionsActions = {
+  requestQuestions,
+  nextQuestion,
+}
+
+export type QuestionsAction = ActionType<typeof questionsActions>;
+
+export const questionsReducer = createReducer<IQuestionsState, QuestionsAction>(initialQuestionsState)
+  .handleAction(requestQuestions.request, (state) => ({ ...state, loading: true }))
+  .handleAction(requestQuestions.success, (state, action) => {
+    // Shuffle the questions as per spec
+    const questions = shuffle(action.payload);
+    const current = questions.shift() || null;
+    return {
+      current,
+      questions,
+      loading: false,
+    }
+  })
+  .handleAction(requestQuestions.failure, (state) => ({ ...state, loading: false }))
+  .handleAction(nextQuestion, (state, action) => {
+    // Shouldn't be able to skip if still loading
+    if (state.loading) return state;
+
+    const remainingQuestions = state.questions;
+    const nextQuestion = remainingQuestions.shift() || null;
+    return {
+      ...state,
+      current: nextQuestion,
+      questions: remainingQuestions,
+    }
+  });
