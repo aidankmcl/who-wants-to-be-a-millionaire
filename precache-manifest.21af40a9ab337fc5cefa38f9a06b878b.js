4577d25006c4445a11f8ef38ef40874a912a9d94
Aidan <aidankmcl@gmail.com>
2019-12-02 10:44:47 +0100

    Updates

---
+++ precache-manifest.21af40a9ab337fc5cefa38f9a06b878b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3579fc678c21f7d2c300556578fda66",
+    "url": "/who-wants-to-be-a-millionaire/index.html"
+  },
+  {
+    "revision": "cf38b0dacb720760cec9",
+    "url": "/who-wants-to-be-a-millionaire/static/css/main.040d7541.chunk.css"
+  },
+  {
+    "revision": "5598fcae2710e0670705",
+    "url": "/who-wants-to-be-a-millionaire/static/js/2.43989426.chunk.js"
+  },
+  {
+    "revision": "cf38b0dacb720760cec9",
+    "url": "/who-wants-to-be-a-millionaire/static/js/main.f607e7e7.chunk.js"
+  },
+  {
+    "revision": "9d34b0705ad0831efc84",
+    "url": "/who-wants-to-be-a-millionaire/static/js/runtime-main.a02289cb.js"
+  }
+]);
